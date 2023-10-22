e6311b500c44795cfd305b0c9252ee0c74d64c54
Alan Graham <alan@nettica.com>
2023-10-22 08:07:07 -0700

    remove dead code

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
 import vuetify from "./plugins/vuetify";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@mdi/font/css/materialdesignicons.min.css";
@@ -14,7 +13,6 @@ Vue.config.productionTip = false;
 new Vue({
   el: "#app",
   router,
-  store,
   vuetify,
   VueApexCharts,
   render: (h) => h(App),
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -6,7 +6,7 @@
 import MainView from "../components/MainView";
 
 export default {
-  name: "HomeView",
+  name: "MainView",
 
   components: {
     MainView,
