d20e36beb9ecab0dc4ff07ae8205d57676086dcf
Alan Graham <alan@nettica.com>
2023-10-25 07:47:42 +0000

    release

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+export NODE_OPTIONS=--openssl-legacy-provider
 npm run electron:build
 npm run electron:build -- --linux rpm --arm64 --dir   # arm64 cross compile
 npm run electron:build -- --linux deb --arm64 --dir   # arm64 cross compile
