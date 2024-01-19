dbc666aa44fe5089d966f1adcf0b21b528365074
Alan Graham <alan@nettica.com>
2024-01-18 18:24:25 -0800

    logout for client

--- src/background.js
+++ src/background.js
@@ -110,7 +110,7 @@ ipcMain.on("logout", () => {
     height: 600,
     show: false,
   });
-  win.loadURL("https://" + device.server + "/api/v1.0/auth/logout");
+  win.loadURL("https://" + device.server + "/api/v1.0/auth/logout?redirect_url=com.nettica.agent://callback/agent");
   authService.logout();
 
   win.on("ready-to-show", () => {
